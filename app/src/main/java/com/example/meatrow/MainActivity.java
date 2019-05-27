0e0e238dbcc4530bf0a9d19eaf816f98ea561e35
LunXNeko <artwarchol@gmail.com>
2019-05-28 02:30:36 +0300

    Add FirebaseDB Func

--- app/src/main/java/com/example/meatrow/MainActivity.java
+++ app/src/main/java/com/example/meatrow/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.meatrow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -108,5 +109,10 @@ public class MainActivity extends AppCompatActivity {
 //        //String[] datas = {"choto1", "choto2"};
 //        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datas);
 //        clubsList.setAdapter(adapter);
+    }
+
+    public void testActivity(View view){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
