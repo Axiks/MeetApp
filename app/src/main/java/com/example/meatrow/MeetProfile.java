1c7451887fb022fd872d18cdeb11c0ae93e21a8a
LunXNeko <artwarchol@gmail.com>
2019-05-30 23:14:32 +0300

    Prirmitive Avatar Meet Load

--- app/src/main/java/com/example/meatrow/MeetProfile.java
+++ app/src/main/java/com/example/meatrow/MeetProfile.java
@@ -1,7 +1,10 @@
 package com.example.meatrow;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,14 +12,19 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,6 +33,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
@@ -33,16 +42,17 @@ import java.io.File;
 import java.nio.file.FileStore;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class MeetProfile extends AppCompatActivity {
     private static final  int PICK_IMAGE_REQURET = 1;
     private static final String TAG = "Neko";
     public FirebaseAuth uAuth;
-    public DatabaseReference refMeets;
+    public DatabaseReference refMeets;//
     public MeetAdapter meetAdapter;
     public Meet meet;
-    public StorageReference mStorageRef;
-    private Uri mImageUri;
+    public StorageReference mStorageRef;//
+    private Uri mImageUri;//
 
     TextView tvCreateDate;
     TextView tvCreator;
@@ -75,7 +85,8 @@ public class MeetProfile extends AppCompatActivity {
         imageAvatar = findViewById(R.id.imageAvatar);
         recTeams = findViewById(R.id.recTeams);
 
-        mStorageRef = FirebaseStorage.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+
         Bundle extras = getIntent().getExtras();
         String meetID = extras.getString("meetId");
 
@@ -116,8 +127,8 @@ public class MeetProfile extends AppCompatActivity {
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //uploadImage();
                 openFileChhooser();
+
             }
         });
     }
@@ -138,13 +149,56 @@ public class MeetProfile extends AppCompatActivity {
 
         if (requestCode == PICK_IMAGE_REQURET && resultCode == RESULT_OK && resultCode == RESULT_OK
         && data != null && data.getData() != null){
-            mImageUri = data.getData();
+            mImageUri = data.getData(); //patch
 
             Picasso.get().load(mImageUri).into(imageAvatar);
         }
     }
 
-    public void uploadImage(){
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    //update user info
+    private void uploadImage(){
+        if(mImageUri != null){
+            StorageReference reference = mStorageRef.child("images/" + UUID.randomUUID().toString());
+            reference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(MeetProfile.this, "Upload succeful", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+//            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()+ "."
+//                    + getFileExtension(mImageUri));
+//            fileReference.putFile(mImageUri)
+//                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                            Toast.makeText(MeetProfile.this, "Upload succeful", Toast.LENGTH_SHORT).show();
+//                            Upload upload = new Upload("File namee", taskSnapshot. );
+//                        }
+//                    })
+//                    .addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception e) {
+//                            Toast.makeText(MeetProfile.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//                        }
+//                    })
+//                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+//                @Override
+//                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+//                            //Progress bar
+//                        }
+//                    });
+
+        }else {
+            Toast.makeText(this, "No file seelcted", Toast.LENGTH_SHORT).show();
+        }
 //        Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
 //        StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
 //
@@ -170,6 +224,10 @@ public class MeetProfile extends AppCompatActivity {
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, PICK_IMAGE_REQURET);
+    }
+
+    public void Tets(View view){
+        uploadImage();
     }
     //Create data
     //Autor id create
--- app/src/main/java/com/example/meatrow/Upload.java
+++ app/src/main/java/com/example/meatrow/Upload.java
@@ -1,5 +1,38 @@
 package com.example.meatrow;
 
+import java.sql.DataTruncation;
+
 public class Upload {
+    private String mName;
+    private String mImageUrl;
+
+    public Upload(){
+        //empty
+    }
+
+    public Upload(String name, String imageUrl){
+
+        if(name.trim().equals("")){
+            name = "NoName";
+        }
+        mName = name;
+        imageUrl = imageUrl;
+    }
+
+    public String getmName(){
+        return mName;
+    }
+
+    public void setmName(String name){
+        mName = name;
+    }
+
+    public String getImageUri(){
+        return mImageUrl;
+    }
+
+    public void srtImageUri(String imageUrl){
+        mImageUrl = imageUrl;
+    }
 
 }
